ec10234a10b5880471ab84da07becdf9dd99f593
FIRST_NAME LAST_NAME <my_name@example.com>
2024-04-07 10:47:52 +0400

    6: Add request login on supports() method and implement authenticate() method

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -7,19 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
-        dd('supports');
+        return $request->getPathInfo() === '/login' && $request->isMethod('POST');        
     }
 
     public function authenticate(Request $request): PassportInterface
     {
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        
+       return new Passport(
+            new UserBadge($email),
+            new CustomCredentials(
+                function ($credentials, UserInterface $user) {
+                    dd($credentials, $user);
+                }, $password
+            )
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
